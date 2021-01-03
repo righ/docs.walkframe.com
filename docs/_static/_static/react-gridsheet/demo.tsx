cbb2e617565b095e89cd849e080dca54344f0a78
righ <django.hamster@gmail.com>
2021-01-04 00:52:31 +0900

    del

--- docs/_static/_static/react-gridsheet/demo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { Simple } from "./simple";
-
-window.onload = () => {
-  ReactDOM.render(<Simple />, document.querySelector("#demo-simple"));
-};
